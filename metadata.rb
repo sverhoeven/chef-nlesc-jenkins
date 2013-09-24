b33aaa6d3cfdf7d5cc367183d8c809e5bbd7e284
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2013-09-24 16:54:02 +0200

    Up version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@esciencecenter.nl'
 license          'Apache 2.0'
 description      'Installs/Configures nlesc-jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "nlesc-base"
 depends "jenkins"
